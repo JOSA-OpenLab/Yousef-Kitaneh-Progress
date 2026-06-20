8da3d59644b643bf27374f2cfc34e87654c6bd59
dotacow <ykitaneh22@gmail.com>
2026-06-20 11:18:45 +0300

    chore: remove autocompletion testing script

--- test.bash
+++
@@ -1,6 +0,0 @@
-_cli_completion() {
-	    local _clap_line="${COMP_LINE}"
-	        # The vulnerable line introduced in PR 6364:
-		    eval "local _clap_words=($_clap_line)"
-		     }
-		     complete -F _cli_completion cli
